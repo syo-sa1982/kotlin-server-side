0266d4735b94933f0b23f0ba7eaab44fedb16347
fujiiyousuke <yosuke.fujii@nextinnovation-inc.co.jp>
2020-02-20 00:15:37 +0900

    DSLを使ったFunctionalプログラミングでAPIルーター実装＆折角作ったServiceクラスを作ってみる

--- src/main/kotlin/com/example/kotlinserverside/controller/item/ItemController.kt
+++ src/main/kotlin/com/example/kotlinserverside/controller/item/ItemController.kt
@@ -5,19 +5,19 @@ import com.example.kotlinserverside.repository.item.ItemRepository
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 
-@RestController
-class ItemController(private val itemRepository: ItemRepository) {
-    @GetMapping("/items")
-    suspend fun getList() = itemRepository.findAll()
-
-    @GetMapping("/items/{id}")
-    suspend fun getById(@PathVariable id: Int) = itemRepository.findById(id)
-
-    @PostMapping("/items")
-    @ResponseStatus(HttpStatus.CREATED)
-    suspend fun create(@RequestBody item: Item): Item = itemRepository.save(item)
-
-    @DeleteMapping("/items/{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    suspend fun deleteById(@PathVariable id: Int) = itemRepository.deleteById(id)
-}
+//@RestController
+//class ItemController(private val itemRepository: ItemRepository) {
+//    @GetMapping("/items")
+//    suspend fun getList() = itemRepository.findAll()
+//
+//    @GetMapping("/items/{id}")
+//    suspend fun getById(@PathVariable id: Int) = itemRepository.findById(id)
+//
+//    @PostMapping("/items")
+//    @ResponseStatus(HttpStatus.CREATED)
+//    suspend fun create(@RequestBody item: Item): Item = itemRepository.save(item)
+//
+//    @DeleteMapping("/items/{id}")
+//    @ResponseStatus(HttpStatus.NO_CONTENT)
+//    suspend fun deleteById(@PathVariable id: Int) = itemRepository.deleteById(id)
+//}
